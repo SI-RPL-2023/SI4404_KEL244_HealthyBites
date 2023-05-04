c4bda7e1e3fe49171567285f8e41d7d13cf2571d
faraztanjung <mfatanjung@gmail.com>
2023-05-04 11:47:02 +0700

    main.blade.php v2

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -69,9 +69,8 @@
         <!--end::Theme mode setup on page load-->
     </head>
     <!--end::Head-->
-
     <body id="kt_body" class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled aside-fixed aside-default-enabled">
-    <div class="preload-wrapper preloader">
+        <div class="preload-wrapper preloader">
             <div id="preloader_3"></div>
         </div>
         <!--begin::Root-->
@@ -115,5 +114,145 @@
             </div>
             <!--end::page-->
         </div>
+
+        <!--begin::Chat drawer-->
+        <div id="kt_drawer_chat" class="bg-body" data-kt-drawer="true" data-kt-drawer-name="chat" data-kt-drawer-activate="true" data-kt-drawer-overlay="true" data-kt-drawer-width="{default:'300px', 'md': '500px'}" data-kt-drawer-direction="end"data-kt-drawer-toggle="#kt_drawer_chat_toggle" data-kt-drawer-close="#kt_drawer_chat_close">
+
+            <!--begin::Messenger-->
+            <div class="card w-100 rounded-0 border-0" id="kt_drawer_chat_messenger">
+                <!--begin::Card header-->
+                <div class="card-header pe-5" id="kt_drawer_chat_messenger_header">
+                    <!--begin::Title-->
+                    <div class="card-title">
+                        <!--begin::User-->
+                        <div class="d-flex justify-content-center flex-column me-3">
+                            <a href="#" class="fs-4 fw-bold text-gray-900 text-hover-primary me-1 mb-2 lh-1">Brian Cox</a>
+
+                            <!--begin::Info-->
+                            <div class="mb-0 lh-1">
+                                <span class="badge badge-success badge-circle w-10px h-10px me-1"></span>
+                                <span class="fs-7 fw-semibold text-muted">Active</span>
+                            </div>
+                            <!--end::Info-->
+                        </div>
+                        <!--end::User-->
+                    </div>
+                    <!--end::Title-->
+
+                    <!--begin::Card toolbar-->
+                    <div class="card-toolbar">
+                        <!--begin::Menu-->
+                        <div class="me-2">
+                            <button class="btn btn-sm btn-icon btn-active-light-primary" data-kt-menu-trigger="click"
+                                data-kt-menu-placement="bottom-end">
+                                <i class="bi bi-three-dots fs-3"></i>
+                            </button>
+
+                            <!--begin::Menu 3-->
+                            <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg-light-primary fw-semibold w-200px py-3"
+                                data-kt-menu="true">
+                                <!--begin::Heading-->
+                                <div class="menu-item px-3">
+                                    <div class="menu-content text-muted pb-2 px-3 fs-7 text-uppercase">
+                                        Contacts
+                                    </div>
+                                </div>
+                                <!--end::Heading-->
+
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3">
+                                    <a href="#" class="menu-link px-3" data-bs-toggle="modal"
+                                        data-bs-target="#kt_modal_users_search">
+                                        Add Contact
+                                    </a>
+                                </div>
+                                <!--end::Menu item-->
+
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3">
+                                    <a href="#" class="menu-link flex-stack px-3" data-bs-toggle="modal"
+                                        data-bs-target="#kt_modal_invite_friends">
+                                        Invite Contacts
+
+                                        <i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip"
+                                            title="Specify a contact email to send an invitation"></i>
+                                    </a>
+                                </div>
+                                <!--end::Menu item-->
+
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3" data-kt-menu-trigger="hover"
+                                    data-kt-menu-placement="right-start">
+                                    <a href="#" class="menu-link px-3">
+                                        <span class="menu-title">Groups</span>
+                                        <span class="menu-arrow"></span>
+                                    </a>
+
+                                    <!--begin::Menu sub-->
+                                    <div class="menu-sub menu-sub-dropdown w-175px py-4">
+                                        <!--begin::Menu item-->
+                                        <div class="menu-item px-3">
+                                            <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" title="Coming soon">
+                                                Create Group
+                                            </a>
+                                        </div>
+                                        <!--end::Menu item-->
+
+                                        <!--begin::Menu item-->
+                                        <div class="menu-item px-3">
+                                            <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" title="Coming soon">
+                                                Invite Members
+                                            </a>
+                                        </div>
+                                        <!--end::Menu item-->
+
+                                        <!--begin::Menu item-->
+                                        <div class="menu-item px-3">
+                                            <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" title="Coming soon">
+                                                Settings
+                                            </a>
+                                        </div>
+                                        <!--end::Menu item-->
+                                    </div>
+                                    <!--end::Menu sub-->
+                                </div>
+                                <!--end::Menu item-->
+
+                                <!--begin::Menu item-->
+                                <div class="menu-item px-3 my-1">
+                                    <a href="#" class="menu-link px-3" data-bs-toggle="tooltip" title="Coming soon">
+                                        Settings
+                                    </a>
+                                </div>
+                                <!--end::Menu item-->
+                            </div>
+                            <!--end::Menu 3-->
+                        </div>
+                        <!--end::Menu-->
+
+                        <!--begin::Close-->
+                        <div class="btn btn-sm btn-icon btn-active-light-primary" id="kt_drawer_chat_close">
+                            <!--begin::Svg Icon | path: icons/duotune/arrows/arr061.svg-->
+                            <span class="svg-icon svg-icon-2"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
+                                        transform="rotate(-45 6 17.3137)" fill="currentColor" />
+                                    <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
+                                        fill="currentColor" />
+                                </svg>
+
+                            </span>
+                            <!--end::Svg Icon-->
+                        </div>
+                        <!--end::Close-->
+                    </div>
+                    <!--end::Card toolbar-->
+                </div>
+                <!--end::Card header-->
+
+            </div>
+            <!--end::Messenger-->
+        </div>
+        <!--end::Chat drawer-->
     </body>
 </html>
